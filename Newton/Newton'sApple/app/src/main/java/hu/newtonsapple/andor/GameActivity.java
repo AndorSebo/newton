9071f7862ca691f7afb0a834d658db5cf59f84a4
AndorSebo <andorsebo@gmail.com>
2017-08-14 09:53:27 +0200

    Two apple on same time

--- Newton/Newton'sApple/app/src/main/java/hu/newtonsapple/andor/GameActivity.java
+++ Newton/Newton'sApple/app/src/main/java/hu/newtonsapple/andor/GameActivity.java
@@ -14,6 +14,7 @@ import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
@@ -34,12 +35,12 @@ public class GameActivity extends AppCompatActivity {
     ObjectAnimator animator;
 
     ImageView[] apples = new ImageView[10];
-    int height;
-    int width;
+    int height, width, fell;
     ObjectAnimator appleAnimator;
     boolean finished = false;
     SharedPreferences prefs;
     CountDownTimer ct;
+    int speed = 1050;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +49,10 @@ public class GameActivity extends AppCompatActivity {
         newton = (ImageView) findViewById(R.id.newton);
         animation = (AnimationDrawable) newton.getDrawable();
         animation.stop();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         overridePendingTransition(R.anim.scale_from_corner, R.anim.scale_to_corner);
 
-        prefs  = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
         lifeTV = (TextView) findViewById(R.id.heartTV);
         lifeTV.setText(String.valueOf(life));
@@ -69,7 +70,7 @@ public class GameActivity extends AppCompatActivity {
         display.getSize(size);
         height = size.y;
         width = size.x;
-
+        fell = 0;
 
         ct = new CountDownTimer(4000, 500) {
             public void onTick(long millisUntilFinished) {
@@ -77,12 +78,13 @@ public class GameActivity extends AppCompatActivity {
                 leftArrow.setEnabled(false);
                 counter.setText(String.valueOf(millisUntilFinished / 1000));
             }
+
             public void onFinish() {
                 rightArrow.setEnabled(true);
                 leftArrow.setEnabled(true);
                 counter.setVisibility(View.GONE);
                 finished = true;
-                fallApple();
+                fallLot();
             }
         }.start();
 
@@ -97,14 +99,14 @@ public class GameActivity extends AppCompatActivity {
                         animation.selectDrawable(0);
                         break;
                     case MotionEvent.ACTION_DOWN:
-                        if (newton.getX() != height-(rightArrow.getWidth()/3)){
+                        if (newton.getX() != height - (rightArrow.getWidth() / 3)) {
                             leftArrow.setEnabled(false);
-                            animator = ObjectAnimator.ofFloat(newton,"x",height-(rightArrow.getWidth()/3));
+                            animator = ObjectAnimator.ofFloat(newton, "x", height - (rightArrow.getWidth() / 3));
                             newton.setScaleX(1f);
                             animation = (AnimationDrawable) newton.getDrawable();
                             animation.start();
-                            if(!animator.isPaused())
-                                animator.setDuration(1100).start();
+                            if (!animator.isPaused())
+                                animator.setDuration(speed).start();
                             else
                                 animator.resume();
                         }
@@ -126,13 +128,13 @@ public class GameActivity extends AppCompatActivity {
                         break;
                     case MotionEvent.ACTION_DOWN:
                         rightArrow.setEnabled(false);
-                        if (newton.getX() != rightArrow.getWidth()/10){
-                            animator = ObjectAnimator.ofFloat(newton,"x",rightArrow.getWidth()/10);
+                        if (newton.getX() != rightArrow.getWidth() / 10) {
+                            animator = ObjectAnimator.ofFloat(newton, "x", rightArrow.getWidth() / 10);
                             newton.setScaleX(-1f);
                             animation = (AnimationDrawable) newton.getDrawable();
                             animation.start();
-                            if(!animator.isPaused())
-                                animator.setDuration(1100).start();
+                            if (!animator.isPaused())
+                                animator.setDuration(speed).start();
                             else
                                 animator.resume();
                         }
@@ -145,7 +147,7 @@ public class GameActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if(finished)
+        if (finished)
             Alerts.alertToMenu(GameActivity.this, appleAnimator);
         else {
             ct.cancel();
@@ -155,65 +157,80 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
-    private void fallApple(){
-        int sldAppleId;
-        final float appleY;
-        Random rn = new Random();
-        final Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    private void fallLot() {
+        int[] sldAppleId = new int[2];
 
-        for(int i=0; i<apples.length;i++){
-            int resID = getResources().getIdentifier("fall"+(i+1), "id", getPackageName());
+        for (int i = 0; i < apples.length; i++) {
+            int resID = getResources().getIdentifier("fall" + (i + 1), "id", getPackageName());
             apples[i] = (ImageView) findViewById(resID);
         }
 
-        if(life > 0) {
-            sldAppleId = randomApple(apples.length);
-            final ImageView apple = apples[sldAppleId];
-            appleY = apple.getY();
-            final int fallLife = fallLife();
-            if(fallLife <6)
-                apple.setBackgroundResource(R.drawable.heart);
-            else
-                apple.setBackgroundResource(R.drawable.ic_apple);
-
-            appleAnimator = ObjectAnimator.ofFloat(apple, "y", height);
-            appleAnimator.setDuration(rn.nextInt(250)+1050).start();
-            final boolean[] collided = {false};
-            appleAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    if (collision(apple, newton) && !collided[0]) {
-                        collided[0] = true;
-                        if(fallLife>5) {
-                            life--;
-                            if (prefs.getBoolean("vibrate", false))
-                                v.vibrate(150);
-                        }else
-                            life++;
-                        apple.setBackgroundResource(0);
-                        lifeTV.setText(String.valueOf(life));
-                    }
-                }
-            });
-            appleAnimator.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    if (!collided[0]) {
-                        if (fallLife>5)
-                            point++;
-                        pointTV.setText(String.valueOf(point));
-                    }
-                    apple.setBackgroundResource(0);
-                    apple.setY(appleY);
-                    fallApple();
-                }
-            });
+        if (life > 0) {
+            for (int i = 0; i < sldAppleId.length; i++) {
+                sldAppleId[i] = randomApple(apples.length);
+                while (sldAppleId[0] == sldAppleId[1])
+                    sldAppleId[1] = randomApple(apples.length);
+                fallApple(sldAppleId[i]);
+            }
         }else{
-            if(animator != null)
+            if (animator != null)
                 animator.pause();
             newton.setImageResource(R.drawable.newton_dead);
-            Alerts.alertToEnd(GameActivity.this,point);
+            Alerts.alertToEnd(GameActivity.this, point);
         }
+    }
+
+    private void fallApple(int sldAppleId) {
+
+        final float appleY;
+        Random rn = new Random();
+        final Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        final ImageView apple = apples[sldAppleId];
+        appleY = apple.getY();
+        final int fallLife = fallLife();
+
+        if (fallLife < 6)
+            apple.setBackgroundResource(R.drawable.heart);
+        else
+            apple.setBackgroundResource(R.drawable.ic_apple);
+
+        appleAnimator = ObjectAnimator.ofFloat(apple, "y", height);
+        appleAnimator.setDuration(rn.nextInt(250) + 1050).start();
+        final boolean[] collided = {false};
+        appleAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                if (collision(apple, newton) && !collided[0]) {
+                    if (fallLife > 5) {
+                        life--;
+                        if (prefs.getBoolean("vibrate", false))
+                            v.vibrate(150);
+                    } else
+                        life++;
+                    apple.setBackgroundResource(0);
+                    lifeTV.setText(String.valueOf(life));
+                    collided[0] = true;
+
+                }
+            }
+        });
+        appleAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (!collided[0]) {
+                    if (fallLife > 5)
+                        point++;
+                    pointTV.setText(String.valueOf(point));
+                }
+                apple.setBackgroundResource(0);
+                apple.setY(appleY);
+                fell++;
+                if (fell == 2){
+                    fell=0;
+                    fallLot();
+                }
+            }
+        });
     }
 
     private int fallLife(){
